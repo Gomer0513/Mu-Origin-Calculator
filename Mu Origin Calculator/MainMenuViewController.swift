802a8722174c47add0515c31eb04687da8789c14
Gomer0513 <Gomer0513@gmail.com>
2017-07-13 16:43:31 +0300

    implemented ViewControllers for 4 classes

---
+++ Mu Origin Calculator/MainMenuViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainMenuViewController.swift
+//  Mu Origin Calculator
+//
+//  Created by Oleksandr Kysil on 7/13/17.
+//  Copyright © 2017 Oleksandr Kysil. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
