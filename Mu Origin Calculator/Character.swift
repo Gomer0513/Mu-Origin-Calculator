21252bc792f31834cb9b81a8972f7ad1cca847e1
Gomer0513 <Gomer0513@gmail.com>
2017-08-06 17:21:33 +0300

    Implemented stats calculator for all classes.

---
+++ Mu Origin Calculator/Character.swift
@@ -0,0 +1,120 @@
+//
+//  Character.swift
+//  Mu Origin Calculator
+//
+//  Created by Oleksandr Kysil on 7/13/17.
+//  Copyright © 2017 Oleksandr Kysil. All rights reserved.
+//
+
+import Foundation
+
+typealias Point = Int
+
+enum Stats: Point {
+    case str
+    case eng
+    case agi
+    case sta
+}
+
+class Character {
+    var level: Int = 1
+    var rebirth: Int = 0
+    var stats: Stats?
+    var fruits: Point = 0
+    
+    init (rebirth: Int?, level: Int?, fruits: Point?) {
+        self.rebirth = rebirth ?? 0
+        self.level = level ?? 1
+        self.fruits = fruits ?? 0
+    }
+    
+    required init () {}
+    
+    func calculateStats() -> Point {
+        var overalStats = 0
+        var i = 0
+        
+        while i <= self.rebirth {
+            switch i {
+            case 0:
+                if i == self.rebirth {
+                    overalStats = overalStats + (self.level - 1)*5
+                } else {
+                    overalStats = overalStats + 495
+                }
+            case 1:
+                if i == self.rebirth {
+                    overalStats = overalStats + (self.level - 1)*6
+                } else {
+                    overalStats = overalStats + 99*6
+                }
+            case 2:
+                if i == self.rebirth {
+                    overalStats = overalStats + (self.level - 1)*7
+                } else {
+                    overalStats = overalStats + 99*7
+                }
+            case 3:
+                if i == self.rebirth {
+                    overalStats = overalStats + (self.level - 1)*8
+                } else {
+                    overalStats = overalStats + 99*8
+                }
+            case 4:
+                if i == self.rebirth {
+                    overalStats = overalStats + (self.level - 1)*9
+                } else {
+                    overalStats = overalStats + 99*9
+                }
+            case 5:
+                if i == self.rebirth {
+                    overalStats = overalStats + (self.level - 1)*10
+                } else {
+                    overalStats = overalStats + 99*10
+                }
+            case 6:
+                if i == self.rebirth {
+                    overalStats = overalStats + (self.level - 1)*11
+                } else {
+                    overalStats = overalStats + 99*11
+                }
+            case 7:
+                if i == self.rebirth {
+                    overalStats = overalStats + (self.level - 1)*12
+                } else {
+                    overalStats = overalStats + 99*12
+                }
+            case 8:
+                if i == self.rebirth {
+                    overalStats = overalStats + (self.level - 1)*13
+                } else {
+                    overalStats = overalStats + 99*13
+                }
+            case 9:
+                if i == self.rebirth {
+                    overalStats = overalStats + (self.level - 1)*14
+                } else {
+                    overalStats = overalStats + 99*14
+                }
+            case 10:
+                if i == self.rebirth {
+                    overalStats = overalStats + (self.level - 1)*15
+                } else {
+                    overalStats = overalStats + 99*15
+                }
+            default:
+                print("unknown rebirth")
+            }
+            i = i + 1
+        }
+        
+        return overalStats
+    }
+    
+    func calculateFullStats() -> Point {
+        let sum: Point = self.calculateStats() + self.fruits
+        return sum
+    }
+    
+}
--- Mu Origin Calculator/DarkKnightViewController.swift
+++ Mu Origin Calculator/DarkKnightViewController.swift
@@ -8,28 +8,233 @@
 
 import UIKit
 
-class DarkKnightViewController: UIViewController {
+class DarkKnightViewController: UIViewController, UITextFieldDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - Outlets
+    @IBOutlet weak var rebirthInput: UITextField!
+    @IBOutlet weak var levelInput: UITextField!
+    @IBOutlet weak var fruitStatsInput: UITextField!
+    
+    @IBOutlet weak var strField: UITextField!
+    @IBOutlet weak var agiField: UITextField!
+    @IBOutlet weak var staField: UITextField!
+    
+    @IBOutlet weak var totalStats: UILabel!
+    @IBOutlet weak var statsWithoutCreatons: UILabel!
+    
+    @IBOutlet weak var showStatsView: UIView!
+    @IBOutlet weak var inputStatsView: UIView!
+    
+    @IBOutlet weak var calculateButtonOutlet: UIButton!
+    
+    //MARK: - Variables
+    var character = Character()
+    private var totalPoint = Int()
+    private var points = Int()
+    private var str = 0
+    private var eng = 0
+    private var agi = 0
+    private var sta = 0
+    
+    // MARK: - Actions
+    @IBAction func calculateButton(_ sender: Any) {
+        
+        self.character = Character(rebirth: Int(self.rebirthInput.text!), level: Int(self.levelInput.text!), fruits: Int(self.fruitStatsInput.text!))
+        
+        if self.showStatsView.isHidden {
+            self.showStatsView.isHidden = false
+        }
+        if self.inputStatsView.isHidden {
+            self.inputStatsView.isHidden = false
+        }
+        
+        self.totalPoint = self.character.calculateFullStats()
+        self.points = self.character.calculateStats()
+        
+        self.statsWithoutCreatons.text = String(self.points)
+        self.totalStats.text = String(self.totalPoint)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.showStatsView.isHidden = true
+        self.inputStatsView.isHidden = true
+        self.calculateButtonOutlet.isEnabled = false
+        
+        self.rebirthInput.delegate = self
+        self.levelInput.delegate = self
+        self.fruitStatsInput.delegate = self
+        
+        self.strField.delegate = self
+        self.agiField.delegate = self
+        self.staField.delegate = self
+        
     }
-    */
-
+    
+    private func updateStats(_ textField: String?, stat: Stats) {
+        
+        if !((textField?.isEmpty)!) && validationForCreatons(textField) {
+            if stat == .str {
+                self.str = Int(textField!)!
+                self.totalPoint = self.totalPoint - self.str
+            }
+            if stat == .agi {
+                self.agi = Int(textField!)!
+                self.totalPoint = self.totalPoint - self.agi
+            }
+            if stat == .sta {
+                self.sta = Int(textField!)!
+                self.totalPoint = self.totalPoint - self.sta
+            }
+            self.totalStats.text = String(self.totalPoint)
+        }
+        
+        if (textField == "0" || (textField?.isEmpty)!) && stat == .str {
+            self.str = 0
+            self.totalPoint = self.character.calculateFullStats() - self.str - self.agi - self.sta
+            self.totalStats.text = String(self.totalPoint)
+        }
+        if (textField == "0" || (textField?.isEmpty)!) && stat == .agi {
+            self.agi = 0
+            self.totalPoint = self.character.calculateFullStats() - self.str - self.agi - self.sta
+            self.totalStats.text = String(self.totalPoint)
+        }
+        if (textField == "0" || (textField?.isEmpty)!) && stat == .sta {
+            self.sta = 0
+            self.totalPoint = self.character.calculateFullStats() - self.str - self.agi - self.sta
+            self.totalStats.text = String(self.totalPoint)
+        }
+    }
+    
+    /// Reset all objects in this view to its inital state
+    func resetView(){
+        self.resignFirstResponder()
+        self.closekeyboard()
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        self.rebirthInput.keyboardType = .numberPad
+        self.levelInput.keyboardType = .numberPad
+        self.fruitStatsInput.keyboardType = .numberPad
+        self.strField.keyboardType = .numberPad
+        self.agiField.keyboardType = .numberPad
+        self.staField.keyboardType = .numberPad
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.rebirthInput.resignFirstResponder()
+        self.levelInput.resignFirstResponder()
+        self.fruitStatsInput.resignFirstResponder()
+        self.strField.resignFirstResponder()
+        self.agiField.resignFirstResponder()
+        self.staField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        if !(self.rebirthInput.text?.isEmpty)! && !(self.levelInput.text?.isEmpty)! && self.validationForRebirth(self.rebirthInput.text) && validationForLevels(self.levelInput.text) && validationForCreatons(self.fruitStatsInput.text) {
+            self.calculateButtonOutlet.isEnabled = true
+        } else {
+            self.calculateButtonOutlet.isEnabled = false
+        }
+        
+        
+        if !((self.strField.text?.isEmpty)!) && self.strField.resignFirstResponder() {
+            if Int(self.strField.text!)! > self.totalPoint {
+                self.strField.text = String(self.totalPoint)
+                self.updateStats(self.strField.text, stat: .str)
+            } else {
+                self.updateStats(self.strField.text, stat: .str)
+            }
+        } else if (self.strField.text?.isEmpty)! || self.strField.text == "0" {
+            self.updateStats("0", stat: .str)
+        }
+        
+        if !((self.agiField.text?.isEmpty)!) && self.agiField.resignFirstResponder() {
+            if Int(self.agiField.text!)! > self.totalPoint {
+                self.agiField.text = String(self.totalPoint)
+                self.updateStats(self.agiField.text, stat: .agi)
+            } else {
+                self.updateStats(self.agiField.text, stat: .agi)
+            }
+        } else if (self.agiField.text?.isEmpty)! || self.agiField.text == "0" {
+            self.updateStats("0", stat: .agi)
+        }
+        
+        if !((self.staField.text?.isEmpty)!) && self.staField.resignFirstResponder() {
+            if Int(self.staField.text!)! > self.totalPoint {
+                self.staField.text = String(self.totalPoint)
+                self.updateStats(self.staField.text, stat: .sta)
+            } else {
+                self.updateStats(self.staField.text, stat: .sta)
+            }
+        } else if (self.staField.text?.isEmpty)! || self.staField.text == "0" {
+            self.updateStats("0", stat: .sta)
+        }
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        self.rebirthInput.text = ""
+        self.rebirthInput.resignFirstResponder()
+        self.levelInput.text = ""
+        self.levelInput.resignFirstResponder()
+        self.fruitStatsInput.text = ""
+        self.fruitStatsInput.resignFirstResponder()
+        self.strField.text = ""
+        self.strField.resignFirstResponder()
+        self.agiField.text = ""
+        self.agiField.resignFirstResponder()
+        self.staField.text = ""
+        self.staField.resignFirstResponder()
+        return false
+    }
+    
+    // MARK: Helper Methods
+    func closekeyboard() {
+        self.view.endEditing(true)
+    }
+    
+    // MARK: Touch Events
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        closekeyboard()
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        return true
+    }
+    
+    func validationForRebirth(_ enteredData: String?) -> Bool {
+        
+        guard let enteredData = enteredData else {
+            return false
+        }
+        
+        let format = "[0-9]{1,2}"
+        let formatPredicate = NSPredicate(format:"SELF MATCHES %@", format)
+        return formatPredicate.evaluate(with: enteredData)
+    }
+    
+    func validationForLevels(_ enteredData: String?) -> Bool {
+        
+        guard let enteredData = enteredData else {
+            return false
+        }
+        
+        let format = "(?:(?!0)[0-9]{1,2}|100)"
+        let formatPredicate = NSPredicate(format:"SELF MATCHES %@", format)
+        return formatPredicate.evaluate(with: enteredData)
+    }
+    
+    func validationForCreatons(_ enteredData: String?) -> Bool {
+        
+        guard let enteredData = enteredData else {
+            return false
+        }
+        
+        let format = "[0-9]{1,5}"
+        let formatPredicate = NSPredicate(format:"SELF MATCHES %@", format)
+        return formatPredicate.evaluate(with: enteredData)
+    }
 }
