7a76c91685f0ba68d795423197723e3b93e01370
Gomer0513 <Gomer0513@gmail.com>
2018-12-09 20:06:54 +0200

    Increased rebirth up to 15.

--- Mu Origin Calculator/Character.swift
+++ Mu Origin Calculator/Character.swift
@@ -27,13 +27,13 @@ enum Classes: String {
 }
 
 class Character {
-    var level: Int = 1
-    var rebirth: Int = 0
-    var fruits: Point = 0
-    var goldenSword: Point = 0
-    var goldenCrown: Point = 0
-    var goldenScripture: Point = 0
-    var goldenGrail: Point = 0
+    private var level: Int = 1
+    private var rebirth: Int = 0
+    private var fruits: Point = 0
+    private var goldenSword: Point = 0
+    private var goldenCrown: Point = 0
+    private var goldenScripture: Point = 0
+    private var goldenGrail: Point = 0
     
     private var relictSum: Point = 0
     
@@ -46,108 +46,17 @@ class Character {
     required init () {}
     
     func calculateStats() -> Point {
-        var overalStats = 0
-        var i = 0
-        
-        while i <= self.rebirth {
-            switch i {
-            case 0:
-                if i == self.rebirth {
-                    overalStats = overalStats + (self.level - 1)*5
-                } else {
-                    overalStats = overalStats + 495
-                }
-            case 1:
-                if i == self.rebirth {
-                    overalStats = overalStats + (self.level - 1)*6
-                } else {
-                    overalStats = overalStats + 99*6
-                }
-            case 2:
-                if i == self.rebirth {
-                    overalStats = overalStats + (self.level - 1)*7
-                } else {
-                    overalStats = overalStats + 99*7
-                }
-            case 3:
-                if i == self.rebirth {
-                    overalStats = overalStats + (self.level - 1)*8
-                } else {
-                    overalStats = overalStats + 99*8
-                }
-            case 4:
-                if i == self.rebirth {
-                    overalStats = overalStats + (self.level - 1)*9
-                } else {
-                    overalStats = overalStats + 99*9
-                }
-            case 5:
-                if i == self.rebirth {
-                    overalStats = overalStats + (self.level - 1)*10
-                } else {
-                    overalStats = overalStats + 99*10
-                }
-            case 6:
-                if i == self.rebirth {
-                    overalStats = overalStats + (self.level - 1)*11
-                } else {
-                    overalStats = overalStats + 99*11
-                }
-            case 7:
-                if i == self.rebirth {
-                    overalStats = overalStats + (self.level - 1)*12
-                } else {
-                    overalStats = overalStats + 99*12
-                }
-            case 8:
-                if i == self.rebirth {
-                    overalStats = overalStats + (self.level - 1)*13
-                } else {
-                    overalStats = overalStats + 99*13
-                }
-            case 9:
-                if i == self.rebirth {
-                    overalStats = overalStats + (self.level - 1)*14
-                } else {
-                    overalStats = overalStats + 99*14
-                }
-            case 10:
-                if i == self.rebirth {
-                    overalStats = overalStats + (self.level - 1)*15
-                } else {
-                    overalStats = overalStats + 99*15
-                }
-            case 11:
-                if i == self.rebirth {
-                    overalStats = overalStats + (self.level - 1)*16
-                } else {
-                    overalStats = overalStats + 99*16
-                }
-            case 12:
-                if i == self.rebirth {
-                    overalStats = overalStats + (self.level - 1)*17
-                } else {
-                    overalStats = overalStats + 99*17
-                }
-            case 13:
-                if i == self.rebirth {
-                    overalStats = overalStats + (self.level - 1)*18
-                } else {
-                    overalStats = overalStats + 99*18
-                }
-            case 14:
-                if i == self.rebirth {
-                    overalStats = overalStats + (self.level - 1)*19
-                } else {
-                    overalStats = overalStats + 99*19
-                }
-            default:
-                print("unknown rebirth")
-            }
-            i = i + 1
+      var overalStats = 0
+
+      for currentRebirth in 0...self.rebirth {
+        if currentRebirth == self.rebirth {
+          overalStats = overalStats + (self.level - 1)*(5+currentRebirth)
+        } else {
+          overalStats = overalStats + 99*(5 + currentRebirth)
         }
-        
-        return overalStats
+      }
+
+      return overalStats
     }
     
     func evaluateRelicts(goldenSword: Point? = nil ?? 0, goldenCrown: Point? = nil ?? 0, goldenScripture: Point? = nil ?? 0, goldenGrail: Point? = nil ?? 0) {
--- Mu Origin Calculator/MagicKnightViewController.swift
+++ Mu Origin Calculator/MagicKnightViewController.swift
@@ -655,7 +655,7 @@ class MagicKnightViewController: UIViewController, UITextFieldDelegate, saveData
             return false
         }
         
-        let format = "[0-9]{1}|10|11|12|13|14"
+        let format = "[0-9]{1}|10|11|12|13|14|15"
         let formatPredicate = NSPredicate(format:"SELF MATCHES %@", format)
         return formatPredicate.evaluate(with: enteredData)
     }
